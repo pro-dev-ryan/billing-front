a979b2603c28ad73ab851a45f81b086b51318de0
pro-dev-ryan <azizul.developer@gmail.com>
2023-01-30 13:21:39 +0600

    query set-up done

--- src/Functions/Context/DataContext.js
+++ src/Functions/Context/DataContext.js
@@ -1,8 +1,11 @@
 import React, { createContext } from "react";
+import { useQuery } from "react-query";
+import { infos } from "../SideKick/fetcher";
 
 const DataContext = ({ children }) => {
+  const { data, isLoading, refetch } = useQuery("data", `${infos}`);
   const ContextData = createContext({});
-  const value = {};
+  const value = { data, isLoading, refetch };
   return <ContextData.Provider value={value}>{children}</ContextData.Provider>;
 };
 
